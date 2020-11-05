f2a23ea6d7ec70048285aa3f6e2795929acb9c60
stkuwanda <stkuwanda@gmail.com>
2020-11-05 03:12:42 +0200

    updated heading title in HatsPage and made menu-items navigate programmatically using withRoute Hight Order Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+const HatsPage = () => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+);
+
 function App() {
-  return (
-    <div className="App">
-     <HomePage />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/hats' component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
