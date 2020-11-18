26b82871d586b7b0923eb701119e33169767c667
stkuwanda <stkuwanda@gmail.com>
2020-11-18 11:24:29 +0200

    updated App.js to add firebase-auth open and closing subscription handling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import SigninAndSignupPage from './pages/signin-signup-page/signin-signup-page.component';
 import ShopPage from './pages/shoppage/shoppage.component';
@@ -6,17 +7,38 @@ import { Route, Switch } from 'react-router-dom';
 import { auth } from './firebase/firebase.utils';
 import './App.css';
 
-function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route exact path='/shop' component={ShopPage} />
-				<Route exact path='/signin' component={SigninAndSignupPage} />
-			</Switch>
-		</div>
-	);
+class App extends Component {
+	unsubscribeFromAuth = null;
+
+	constructor(){
+		super();
+		this.state = {currentUser: null};
+	}
+
+	componentDidMount(){
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			console.log(user);
+			this.setState({currentUser: user});
+		});
+	}
+
+	componentWillUnmount(){
+		console.log('Unsubscribing from firebase-auth open subscription');
+		this.unsubscribeFromAuth();//close the open subscription to prevent memory leaks
+	}
+
+	render(){
+		return (
+			<div className='App'>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route exact path='/shop' component={ShopPage} />
+					<Route exact path='/signin' component={SigninAndSignupPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
