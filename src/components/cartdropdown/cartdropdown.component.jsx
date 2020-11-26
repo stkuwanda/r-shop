aa9bfb56feb0d2ba58035572348686b350a4a049
stkuwanda <stkuwanda@gmail.com>
2020-11-27 00:35:09 +0200

    implemented cart-items display inside the cart-dropdown component with redux cartItems state

--- src/components/cartdropdown/cartdropdown.component.jsx
+++ src/components/cartdropdown/cartdropdown.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custombutton/custombutton.component';
 import CartItem from '../cartitem/cartitem.component';
 import './cartdropdown.styles.scss';
 
-const CartDropdown = () => {
-  return (
-    <div className='cart-dropdown'>
-      <div className="cart-items"/>
-      <CustomButton>Go To Checkout</CustomButton>
-    </div>
-  )
-}
+const CartDropdown = ({ cartItems }) => {
+	return (
+		<div className='cart-dropdown'>
+			<div className='cart-items'>
+				{cartItems.map(cartItem => (
+					<CartItem key={cartItem.id} item={cartItem} />
+				))}
+			</div>
+			<CustomButton>Go To Checkout</CustomButton>
+		</div>
+	);
+};
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+
+export default connect(mapStateToProps)(CartDropdown);
