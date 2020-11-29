256c2fa9dd2dc7a413e80f8255e7e93bce252b08
stkuwanda <stkuwanda@gmail.com>
2020-11-29 15:48:36 +0200

    created checkout page

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './checkout.styles.scss';
+
+const CheckoutPage = () => {
+  return (
+    <div>
+      checkout page!!!
+    </div>
+  )
+}
+
+export default CheckoutPage;
