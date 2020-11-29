09804c8b908632afe6b1993fce4cd698b3471dd9
stkuwanda <stkuwanda@gmail.com>
2020-11-29 15:44:09 +0200

    updated the cart-dropdown component to display empty-cart message if no cart items have been added to the cart

--- src/components/cartdropdown/cartdropdown.component.jsx
+++ src/components/cartdropdown/cartdropdown.component.jsx
@@ -10,15 +10,21 @@ const CartDropdown = ({ cartItems }) => {
 	return (
 		<div className='cart-dropdown'>
 			<div className='cart-items'>
-				{cartItems.map(cartItem => (
-					<CartItem key={cartItem.id} item={cartItem} />
-				))}
+				{cartItems.length ? (
+					cartItems.map(cartItem => (
+						<CartItem key={cartItem.id} item={cartItem} />
+					))
+				) : (
+					<span className='empty-message'>Your cart is empty</span>
+				)}
 			</div>
 			<CustomButton>Go To Checkout</CustomButton>
 		</div>
 	);
 };
 
-const mapStateToProps = createStructuredSelector({ cartItems: selectCartItems });
+const mapStateToProps = createStructuredSelector({
+	cartItems: selectCartItems,
+});
 
 export default connect(mapStateToProps)(CartDropdown);
