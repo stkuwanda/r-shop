6b7491f35ebfab2a71348a3a0cb651a831e3ec98
stkuwanda <stkuwanda@gmail.com>
2020-11-29 12:04:45 +0200

    updated cart.selector.js with the select-hidden selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,13 +1,14 @@
 import { createSelector } from 'reselect';
 
-const selectCart = state => state.cart; 
+const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
 	[selectCart],
 	cart => cart.cartItems
 );
 
-export const selectItemCount = createSelector(
-	[selectCartItems],
-	cartItems => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+export const selectItemCount = createSelector([selectCartItems], cartItems =>
+	cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
 );
+
+export const selectHidden = createSelector([selectCart], cart => cart.hidden);
