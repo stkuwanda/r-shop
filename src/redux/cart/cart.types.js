37c548a8f4b0b5336dd7f5d27423081cb0328a93
stkuwanda <stkuwanda@gmail.com>
2020-12-02 08:40:36 +0200

    added new action type REMOVE_ITEM

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   TOGGLE_CART_DROPDOWN: 'TOGGLE_CART_DROPDOWN',
-  ADD_ITEM: 'ADD_ITEM'
+  ADD_ITEM: 'ADD_ITEM',
+  REMOVE_ITEM: 'REMOVE_ITEM'
 };
 
 export default CartActionTypes;
