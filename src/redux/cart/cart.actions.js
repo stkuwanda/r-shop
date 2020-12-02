3578f888700e1735795f48d319ce77853640728b
stkuwanda <stkuwanda@gmail.com>
2020-12-02 08:43:18 +0200

    added a remove-item action

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,7 @@ import CartActionTypes from './cart.types';
 export const toggleCartDropdown = () => ({type: CartActionTypes.TOGGLE_CART_DROPDOWN});
 
 export const addItem = item => ({type: CartActionTypes.ADD_ITEM, payload: item});
+
+export const removeItem = item => ({type: CartActionTypes.REMOVE_ITEM, payload: item});
+
+
