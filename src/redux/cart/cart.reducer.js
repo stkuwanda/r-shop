d97c37de4d20b3ff0062d4ee99a8a5f38f1a25b9
stkuwanda <stkuwanda@gmail.com>
2020-12-03 10:48:31 +0200

    updated the cart.reducer.js, updated cart.types.js

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart, removeCartItem } from './cart.utils';
+import { addItemToCart, removeCartItem, reduceQuantity } from './cart.utils';
 
 const INITIAL_STATE = {
 	hidden: true,
@@ -11,9 +11,20 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case CartActionTypes.TOGGLE_CART_DROPDOWN:
 			return { ...state, hidden: !state.hidden };
 		case CartActionTypes.ADD_ITEM:
-			return { ...state, cartItems: addItemToCart(state.cartItems, action.payload) };
+			return {
+				...state,
+				cartItems: addItemToCart(state.cartItems, action.payload),
+			};
 		case CartActionTypes.REMOVE_ITEM:
-			return { ...state, cartItems: removeCartItem(state.cartItems, action.payload)}
+			return {
+				...state,
+				cartItems: removeCartItem(state.cartItems, action.payload),
+			};
+		case CartActionTypes.REDUCE_ITEM_QTY:
+			return {
+				...state,
+				cartItems: reduceQuantity(state.cartItems, action.payload),
+			};
 		default:
 			return state;
 	}
