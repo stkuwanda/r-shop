f4a8fff832f8f64a8b44e6dbdeb17e8de8cdc88d
stkuwanda <stkuwanda@gmail.com>
2020-12-03 10:29:47 +0200

    updated cart-action-types to include the reduce_qty action type

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,8 @@
 const CartActionTypes = {
   TOGGLE_CART_DROPDOWN: 'TOGGLE_CART_DROPDOWN',
   ADD_ITEM: 'ADD_ITEM',
-  REMOVE_ITEM: 'REMOVE_ITEM'
+  REMOVE_ITEM: 'REMOVE_ITEM',
+  REDUCE_QTY: 'REDUCE_QTY'
 };
 
 export default CartActionTypes;
