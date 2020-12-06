378011eb8bced1b7e473d5f7f12acffc07457e47
stkuwanda <stkuwanda@gmail.com>
2020-12-06 16:43:01 +0200

    added another route to the shop-page

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../category/category.component';
+import './shoppage.styles.scss';
 
 const ShopPage = ({ match }) => {
+	console.log(match);
 	return (
 		<div className='shope-page'>
-			<Route exact path={`${match.url}`} component={CollectionsOverview} />
+			<Route exact path={`${match.path}`} component={CollectionsOverview} />
+			<Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
 		</div>
 	);
 };
