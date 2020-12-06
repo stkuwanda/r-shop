8abdf9b302253c17f9fda6649ed7971617b14f69
stkuwanda <stkuwanda@gmail.com>
2020-12-06 16:05:13 +0200

    created the category-page

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import './category.styles.scss';
+
+const CategoryPage = () => {
+  return (
+    <div className='category'>
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  )
+}
+
+export default CategoryPage;
