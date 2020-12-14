db4a38bfd1d05885918f84c5c3cc811f335adb0d
stkuwanda <stkuwanda@gmail.com>
2020-12-14 14:41:44 +0200

    updated collection-item component

--- src/components/collectionitem/collectionitem.component.jsx
+++ src/components/collectionitem/collectionitem.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import CustomButton from '../custombutton/custombutton.component';
 import { addItem } from '../../redux/cart/cart.actions';
 import './collectionitem.styles.scss';
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item, addItem, category }) => {
+	category = 'product';
 	const {imageUrl, name, price} = item;
 	return (
 		<div className='collection-item'>
@@ -12,6 +14,7 @@ const CollectionItem = ({ item, addItem }) => {
 			<div className='collection-footer'>
 				<span className='name'>{name}</span>
 				<span className='price'>{price}</span>
+				<Link to={`/product/${category}/${name}`} > <span className="go">Go</span></Link>
 			</div>
 			<CustomButton onClick={() => addItem(item)} isInverted>Add To Cart</CustomButton>
 		</div>
